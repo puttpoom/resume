a13668838bfd206f716bed59a57c90a856388838
puttpoom <puttpoom@users.noreply.github.com>
2026-07-14 08:50:15 +0000

    deploy: ae1e57111a4347c387c817c30d6336ddfa4aa6f1

---
+++ _next/static/W-LfHMW4eGwDBMgIpCXVz/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/spa-portfilo//_next/:path+",
+        "destination": "/spa-portfilo/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
